d9637fff7fd6b5135474e0aa7907fc57b3142858
mateusz.loch <mateusz.loch@gmail.com>
2020-10-18 17:15:20 +0200

    Add template example1

--- cpp/template1.cpp
+++ cpp/template1.cpp
@@ -5,9 +5,14 @@ using namespace std;
 int array[] = {1, 2, 3, 4, 5};
 
 void addToArray(int x);
-
+template<typename T> 
+inline void callForMax(const T& a, const T& b){
+	addToArray(a > b ? a : b);
+}
 
 int main(){
+	
+	callForMax(10, 100);
 
 	for (const auto &e : array){
 		cout << e << endl;
